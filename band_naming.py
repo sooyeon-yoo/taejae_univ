28142f11d1bfe5a2e8dc64f68ec429cd9a61349e
sooyeon-yoo <163519803+sooyeon-yoo@users.noreply.github.com>
2024-03-22 23:45:35 +0900

    Update band_naming.py

--- band_naming.py
+++ band_naming.py
@@ -1,4 +1,4 @@
 print("Welcome!")
-BC = str(input("Please type your bottom's color: "))
-LF = str(input("Please type the last food you ate: "))
+BC = str(input("Please enter your bottom's color: "))
+LF = str(input("Please enter the last food you ate: "))
 print("Your band name is ", BC, " ", LF, ".")
